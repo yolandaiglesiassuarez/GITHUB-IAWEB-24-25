67f431803324ab35a761540ce97acacdeb20725a
yolandais <yolandapaulanoa@gmail.com>
2024-11-28 11:07:05 +0100

    sexto

--- prueba.php
+++ prueba.php
@@ -4,5 +4,6 @@ echo "Estamos dentro de visual studio code";
 echo "Segundo commit";
 echo "Tercer commit";
 echo "Cuarto cambio";
-echo "cambio cinco"
+echo "cambio cinco";
+echo "cambio sexto";
 ?>
